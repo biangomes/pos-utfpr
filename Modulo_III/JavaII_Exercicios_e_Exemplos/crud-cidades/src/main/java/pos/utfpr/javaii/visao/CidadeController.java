84aa7a0d90097f4710cae1f6d8a8d8d23879a09b
Beatriz Gomes <beanascigom@gmail.com>
2024-05-01 15:02:14 -0300

    Utilizando o arquivo de estilizacao crud.html

---
+++ Modulo_III/JavaII_Exercicios_e_Exemplos/crud-cidades/src/main/java/pos/utfpr/javaii/visao/CidadeController.java
@@ -0,0 +1,12 @@
+package pos.utfpr.javaii.visao;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class CidadeController {
+    @GetMapping("/")
+    public String listar() {
+        return "crud.html";
+    }
+}
