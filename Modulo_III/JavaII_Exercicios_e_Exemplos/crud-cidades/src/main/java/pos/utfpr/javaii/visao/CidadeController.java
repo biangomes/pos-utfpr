2b129eb3a88dbeb9f1a85332dc9a3ccb2e42de23
Beatriz Nascimento Gomes <beatriz.ngomes@luizalabs.com>
2024-07-21 13:17:15 -0300

    Inserindo persistencia em BD H2

--- Modulo_III/JavaII_Exercicios_e_Exemplos/crud-cidades/src/main/java/pos/utfpr/javaii/visao/CidadeController.java
+++ Modulo_III/JavaII_Exercicios_e_Exemplos/crud-cidades/src/main/java/pos/utfpr/javaii/visao/CidadeController.java
@@ -5,28 +5,66 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.HashSet;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 @Controller
 public class CidadeController {
 
     private Set<Cidade> cidades;
+    private final CidadeRepository repository;
 
-    public CidadeController() {
+    public CidadeController(CidadeRepository repository) {
       cidades = new HashSet<>();
+      this.repository = repository;
     }
 
     @GetMapping("/")
     public String listar(Model memoria) {
-        memoria.addAttribute("listaCidades", cidades);
+        memoria.addAttribute("listaCidades", repository
+            .findAll()
+            .stream()
+            .map(cidade -> new Cidade(
+                cidade.getNome(),
+                cidade.getEstado()))
+            .collect(Collectors.toList()));
         return "/crud";
     }
 
     @PostMapping("/criar")
     public String criar(@RequestBody Cidade cidade) {
-        cidades.add(cidade);
+        var novaCidade = new CidadeEntidade();
+        novaCidade.setNome(cidade.getNome());
+        novaCidade.setEstado(cidade.getEstado());
+        repository.save(novaCidade);
+
         return "redirect:/";
+    }
+
+    @GetMapping("/excluir")
+    public String excluir(@RequestParam String nome, @RequestParam String estado) {
+        var cidadeEstadoEncontrada = repository.findByNomeAndEstado(nome, estado);
+        cidadeEstadoEncontrada.ifPresent(repository::delete);
+
+        return "redirect:/";
+    }
+
+    public String preparaAlterar(@RequestParam String nome, @RequestParam String estado, Model memoria) {
+        var cidadeAtual = cidades
+            .stream()
+            .filter(cidade ->
+                        cidade.getNome().equals(nome) &&
+                        cidade.getEstado().equals(estado))
+            .findAny();
+
+        if (cidadeAtual.isPresent()) {
+            memoria.addAttribute("cidadeAtual", cidadeAtual.get());
+            memoria.addAttribute("listaCidades", cidades);
+        }
+
+        return "/crud";
     }
 }
